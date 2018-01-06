ba57e032661d495d7f74d0946e2f9d1dd001f700
Chris Rutter <ChrisRut@users.noreply.github.com>
2018-01-06 13:32:50 -0500

    CryptoML updates

--- freqtrade/misc.py
+++ freqtrade/misc.py
@@ -283,6 +283,16 @@ def build_subcommands(parser: argparse.ArgumentParser) -> None:
 CONF_SCHEMA = {
     'type': 'object',
     'properties': {
+        'analyzer': {
+            'type': 'object',
+            'properties': {
+                "method": {'type': 'string', 'enum': ['cryptoml']},
+                "threshold": {'type': 'number', 'minimum': 0.001},
+                "repeats": {'type': 'integer'},
+                "api_key": {'type': 'string'},
+                "ttl": {'type': 'integer'}
+            }
+        },
         'max_open_trades': {'type': 'integer', 'minimum': 1},
         'ticker_interval': {'type': 'integer', 'enum': [1, 5, 30, 60, 1440]},
         'stake_currency': {'type': 'string', 'enum': ['BTC', 'ETH', 'USDT']},
